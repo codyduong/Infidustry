2ca607ab64adb6ea8309505d8be336125548bb65
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2020-03-24 15:10:13 -0500

    Update inserter.js

--- scripts/inserter.js
+++ scripts/inserter.js
@@ -1,3 +1,5 @@
+const speed = 8
+
 const inserter = extendContent(Router, "inserter", {
 
 	Router(tile.Name){
@@ -13,13 +15,13 @@ const inserter = extendContent(Router, "inserter", {
 	update(Tile tile){
 		var entity = tile.ent();
 
-		if(entity.lastItem == null && entity.items.total() > 0) enity.items.clear();
+		if(entity.lastItem == null && entity.items.total() > 0) entity.items.clear();
 
 		if(entity.lastItem != null){
-			entity.time += 1f / speed * Time.delta();
+			entity.time += 1 / speed * Time.delta();
 			var target = tile.entity.proximity().get(tile.rotation()*2);
 
-			if(target != null && (entity.time >= 1f || !(target.block() instanceof Router))){
+			if(target != null && (entity.time >= 1 || !(target.block() instanceof Router))){
 				target.block().handleItem(entity.lastItem, target, Edges.getFacingEdge(tile, target));
 				entity.items.remove(entity.lastItem, 1);
 				entity.lastItem = null;
