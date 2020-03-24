f9bff1b394740cca7104c717290447a801d3b6d0
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2020-03-24 02:42:39 -0500

    Update inserter.js

--- scripts/inserter.js
+++ scripts/inserter.js
@@ -1,22 +1,5 @@
-const inserter = extendContent(Router, "inserter", {
-	update(tile){
-		RouterEntity entity = tile.ent();
+const inserter = extendContent(Block, "inserter", {
+	
 
-		if(entity.lastItem == null && entity.items.total() > 0){
-			entity.items.clear();
-		}
-
-		if(entity.lastItem != null){
-			entity.time += 1f / speed * Time.delta();
-			Tile target = tile.entity.proximity().get(tile.rotation*2);
-
-			if(target != null && (entity.time >= 1f || !(target.block() instanceof Router))){
-				target.block().handleItem(entity.lastItem, target, Edges.getFacingEdge(tile, target));
-				entity.items.remove(entity.lastItem, 1);
-				entity.lastItem = null;
-			}
-		}
-	}
-
-})
+)}
 
